a9d36317bafdf6e14abd32f21c55dfcd2c2c947f
kyledsavery <100172150+kyledsavery@users.noreply.github.com>
2022-05-07 15:12:34 -0700

    Updated version of basic live video edge detection

--- liveEdgeDetect.cpp
+++ liveEdgeDetect.cpp
@@ -1,48 +1,87 @@
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc.hpp>
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+#include <opencv2/videoio.hpp>
 #include <iostream>
-#include <vector>
 using namespace cv;
-using namespace std;
 
-Mat src, src_gray, frame;
-Mat dst, detected_edges;
-int lowThreshold = 0;
-
-const char* window_name = "Edge Map";
-const int max_lowThreshold = 100;
+// Initialize constants
+const int THRESHOLD = 50;
+const int MAX_THRESHOLD = 100;
 const int RATIO = 3;
-const int kernel_size = 3;
+const int KERNEL_SIZE = 3;
+const std::string WINDOW_NAME = "Feed";
 
-static void CannyThreshold(int, void*){
+// Declare global image matrices
+Mat frame, src_gray;
+Mat dst, detected_edges;
+
+/*
+* Function Name: cannyDisplay
+* Algorithm: Detects the edges in an image and
+*            creates a new image containing the 
+*            edge data. This image is then displayed
+* Precondition: 'frame' is initilized to an image
+* Postcondition: 'The new image 'dst' is displayed and
+*                shows the edges from 'frame'
+* Exceptions: None
+* Notes: None
+*/
+static void cannyDisplay(){
+    // Initialize 'dst' to have the same dimensions/type as 'frame'
+    dst.create(frame.size(), frame.type());
+
+    // Convert 'frame' from RGB/BGR to gray scale
+    cvtColor(frame, src_gray, COLOR_BGR2GRAY);
+
+    // Blur the image
     blur(src_gray, detected_edges, Size(3, 3));
-    Canny(detected_edges, detected_edges, lowThreshold, lowThreshold* RATIO, kernel_size);
+
+    // Use Canny Edge Detection method to detected the edges
+    Canny(detected_edges, detected_edges, THRESHOLD, THRESHOLD * RATIO, KERNEL_SIZE);
+
+    // Setup destination Mat and display it
     dst = Scalar::all(0);
     frame.copyTo(dst, detected_edges);
-    imshow("Edges", dst);
+    imshow(WINDOW_NAME, dst);
 }
 
-int main(int argc, char** argv)
-{
-    VideoCapture video_src(argv[1]);
+int main(int argc, char** argv){
+    // Open default camera
+    VideoCapture video_src(0);
+
+    // If unsuccessful, end the program
     if (!video_src.isOpened()){
-        std::cout << "coudl not open video" << argv[1] << std::endl;
+        std::cout << "Could not open video source " << argv[1] << std::endl;
         return -1;
     }
-    bool framesLeft = true, frameGrabSuccess;
-    while (framesLeft) {
+
+    // Prime the while loop
+    bool framesLeft = true;
+    bool frameGrabSuccess;
+    int frameWidth = video_src.get(CAP_PROP_FRAME_WIDTH);
+    int frameHeight = video_src.get(CAP_PROP_FRAME_HEIGHT);
+    
+    // While frames are left in the video feed, continue looping
+    while(framesLeft){
+
+        // Grab the next frame from the video
         frameGrabSuccess = video_src.read(frame);
-        if (frameGrabSuccess) {
-            dst.create(frame.size(), frame.type());
-            cvtColor(frame, src_gray, COLOR_BGR2GRAY);
-            namedWindow("Edges", WINDOW_NORMAL);
-            CannyThreshold(0, 0);
-            waitKey(0);
+
+        // If successful detect edges in the frame
+        if(frameGrabSuccess){
+
+            // Using Canny Edge Detection, create a new image and display it
+            cannyDisplay();
+
+            // Use argument = 1 for live video and 0 for single images
+            waitKey(1);
         }
+        // Video has ended
         else {
             framesLeft = false;
         }
     }
+
+    // Terminate the program
     return 0;
 }
